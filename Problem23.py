a1d57e0bbbacd223ead4e58dfdc10d521d3fecb7
Danius Silkaitis <danius.silkaitis@gmail.com>
2016-12-26 11:06:02 -0500

    Solved

---
+++ Problem23.py
@@ -0,0 +1,67 @@
+'''
+A perfect number is a number for which the sum of its proper divisors is
+exactly equal to the number. For example, the sum of the proper divisors of 28
+would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less
+than n and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
+number that can be written as the sum of two abundant numbers is 24.
+By mathematical analysis, it can be shown that all integers greater than
+28123 can be written as the sum of two abundant numbers. However, this upper
+limit cannot be reduced any further by analysis even though it is known that
+the greatest number that cannot be expressed as the sum of two abundant
+numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the
+sum of two abundant numbers.
+'''
+
+def divisors(start, num):
+
+    sum = 0
+
+    for v in xrange(start, 0, -1):
+
+        if num % v == 0:
+            sum += v
+
+    return(sum)
+
+def abundant(usl):
+
+    soln = []
+
+    for num in xrange(usl + 1):
+
+        sum_divs = divisors(num - 1, num)
+
+        if sum_divs > num:
+            soln.append(num)
+
+    return(soln)
+
+def abundant_sums(arr):
+    soln = set()
+
+    for i in xrange(len(arr)):
+
+        for j in xrange(i, len(arr)):
+            soln.add(arr[i] + arr[j])
+
+
+    return(soln)
+
+if __name__ == '__main__':
+    usl = 28123
+
+    abndnt_nums = abundant(usl)
+
+    abndnt_sums = abundant_sums(abndnt_nums)
+
+    ints = set(range(usl))
+
+    print('Sum of all positive integers that' \
+            'are not the sum of two abundant numbers is' \
+            .format(sum(ints - abndnt_sums)))
